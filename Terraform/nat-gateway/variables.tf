8d466ea1096521dbe2eaa445c6d7619cd21d0479
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-09-27 11:18:58 -0400

    Add Terraform configuration for NAT gateway

---
+++ Terraform/nat-gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "allocation_id" {
+  description = "EIP Allocation ID for NAT Gateway"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Public Subnet ID"
+  type        = string
+}
