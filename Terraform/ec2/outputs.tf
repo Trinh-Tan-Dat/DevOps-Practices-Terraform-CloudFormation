a4e4d2bcf415010944c88182ab0253fd70399686
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-10-05 10:51:52 -0400

    testing

--- Terraform/ec2/outputs.tf
+++ Terraform/ec2/outputs.tf
@@ -6,10 +6,10 @@ output "private_instance_id" {
   value = aws_instance.private_instance.id
 }
 
-output "public_ec2_ip" {
-  value = aws_ec2.public_ec2.public_ip
-}
+# output "public_ec2_ip" {
+#   value = aws_ec2.public_ec2.public_ip
+# }
 
-output "private_ec2_ip" {
-  value = aws_ec2.private_ec2.private_ip
-}
+# output "private_ec2_ip" {
+#   value = aws_ec2.private_ec2.private_ip
+# }
